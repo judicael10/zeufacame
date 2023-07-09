c9a7cb6e31486a06ec8083dbeb59899665998b4b
camer <128632619+judicael10@users.noreply.github.com>
2023-07-09 14:56:37 +0100

    Add files via upload

---
+++ appli.js
@@ -0,0 +1,9 @@
+const divcontenaire = document.querySelector('#show');
+
+let show = function(){
+    divcontenaire.style.display = 'block';
+}
+
+let ferme = function(){
+    divcontenaire.style.display = 'none';
+}
